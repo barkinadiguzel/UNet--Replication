01efa801306e050212ad28a8a4a9892e7bb02d8e
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-04 17:02:35 +0300

    Create conv_layer.py

---
+++ src/layers/conv_layer.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ConvLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        return self.relu(self.bn(self.conv(x)))
